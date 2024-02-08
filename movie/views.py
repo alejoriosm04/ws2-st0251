23028b6e73f51e1b3c02809c1c1f5411c39fb046
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-02-08 06:39:46 -0500

    Search movie

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     # return HttpResponse('<h1>Movie Review Home</h1>')
-    return render(request, 'home.html', {'name': 'Alejandro Ríos'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
